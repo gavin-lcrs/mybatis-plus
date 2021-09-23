3fdc3aa4c287cf864d89f135e9a70f1e538228a1
jiwen.cao <jiwen.cao@gaialab.cn>
2021-09-23 14:29:20 +0800

    mybatis-plus 配置Nacos

--- src/main/java/com/gavin/mybatis/controller/UserController.java
+++ src/main/java/com/gavin/mybatis/controller/UserController.java
@@ -3,7 +3,9 @@ package com.gavin.mybatis.controller;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.nacos.api.config.annotation.NacosValue;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.gavin.mybatis.model.entity.Userinfo;
 import com.gavin.mybatis.service.IUserinfoService;
 import io.lettuce.core.dynamic.annotation.Param;
@@ -40,7 +42,8 @@ public class UserController {
 
     @GetMapping("/page")
     public String getUserListByPage(@Param("pageNum") Integer pageNum, @Param("pageSize") Integer pageSize){
-        IPage<Userinfo> result = userinfoService.selectUserPage(pageSize, pageNum);
+        IPage<Userinfo> page = new Page<>(pageNum, pageSize);
+        IPage<Userinfo> result = userinfoService.page(page, new QueryWrapper<>());
         log.info("wsmsg={}", wsmsg);
         return JSONArray.toJSONString(result);
     }
